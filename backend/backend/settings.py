64ce44d6c23a4d62de58114e464ba196aaed6395
Стас Басов <jil.sheladm@yandex.ru>
2023-08-23 00:17:03 -0300

    last settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.18.222']
+ALLOWED_HOSTS = ['jiltret.sytes.net', '127.0.0.1', 'localhost', '51.250.18.222']
 
 
 # Application definition
